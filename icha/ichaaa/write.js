1bc8586416db8504a6b2c4135554096c3084c1e0
ilfaaany <ichahidayat7@gmail.com>
2022-03-14 21:46:22 +0700

    coba commit

---
+++ icha/ichaaa/write.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const isiKonten = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+`;
+
+fs.writeFile(`coba.html`, isiKonten, function (err){
+    iff(err) throw err;
+    console.log(`file html creeated`);
+});
