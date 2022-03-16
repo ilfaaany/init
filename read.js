ac801f58e593c2fd87d0484b71dd7b3d3023bed1
ilfaaany <ichahidayat7@gmail.com>
2022-03-16 16:57:21 +0700

    try eslint

---
+++ read.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+fs.readFile("coba.html", function (err, data){
+    iff(err) throw err;
+    console.log(data.toString('utf-8'));
+    //console.log(data.toJson());
+});
